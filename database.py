8a39313043f1ce999675bbe652b844b2dfbc0c2e
Tshidiso Mahlo <tshidisomahlo05@gmail.com>
2024-06-22 14:25:13 +0200

    new file database.py file

---
+++ database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def create_database():
+    con = sqlite3.connect("StudentSystemv.db")
+    cur = con.cursor()
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS learners (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                age INTEGER,
+                grade TEXT)''')
+    
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS results (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    learner_id INTEGER,
+                    subject TEXT NOT NULL,
+                    score INTEGER,
+                    FOREIGN KEY (learner_id) REFERENCES learners(id))''')
+    
+    con.commit()
+    con.close()
+create_database()
